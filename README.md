86ab65ed044f5300e26d1e65cb39e9f9fda6506d
neharichi <54488933+neharichi@users.noreply.github.com>
2020-01-01 17:45:19 -0800

    initial code with all files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # NoteTaker
+
+## README
+This is a command line application that takes personal notes.
+Application asks user to input note description.
+
+## Command to run application
+node server.js
+
+
+### modules used
+express
+
+
+
+
---
+++ data/tableData.js
@@ -0,0 +1,15 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the notes.
+// Initially we just set it equal to a "dummy" note.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var tableArray = [
+  {  
+    NoteDescription: "My First note :)- "   
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = tableArray;
